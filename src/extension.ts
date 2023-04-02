ccb91d2902bd6f8627187778d1d45ab91ad1c917
kiyonori.matsumoto <kiyonori.matsumoto@sansan.com>
2023-04-03 00:25:20 +0900

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { format } from "./format";
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  const formatterProvider = {
+    provideDocumentFormattingEdits(
+      document: vscode.TextDocument,
+      options: vscode.FormattingOptions,
+      token: vscode.CancellationToken
+    ): vscode.ProviderResult<vscode.TextEdit[]> {
+      if (token.isCancellationRequested) {
+        return [];
+      }
+
+      const formattedText = format(document.getText(), "sqlx");
+
+      const range = new vscode.Range(0, 0, document.lineCount, 0);
+      return [vscode.TextEdit.replace(range, formattedText)];
+    },
+  };
+
+  const selector = { language: "sqlx" };
+
+  context.subscriptions.push(
+    vscode.languages.registerDocumentFormattingEditProvider(
+      selector,
+      formatterProvider
+    )
+  );
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
